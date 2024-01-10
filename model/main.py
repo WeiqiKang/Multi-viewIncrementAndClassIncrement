71676c76cb66c80cb812c757ea3a2452fa28ea8f
wqii <979659702@qq.com>
2024-01-10 20:55:42 +0800

    分类器改用SVM，完成分类器的实现

--- model/main.py
+++ model/main.py
@@ -1,14 +1,9 @@
-import sys
-
 import hdf5storage
 import os
-import torch
-from torch import nn
-from torchvision import transforms
-from tqdm import tqdm
 
-from model import AlexNet
-from torch.utils.data import TensorDataset, DataLoader, random_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
 def read_data(dataset):
@@ -30,62 +25,13 @@ def read_data(dataset):
 
 if __name__ == "__main__":
     X, Y = read_data(dataset="100Leaves")
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    Y = Y.reshape(-1)
+    std = StandardScaler()
+    X_std = std.fit_transform(X)
 
-    data_tensor = torch.tensor(X, dtype=torch.float32)
-    label_tensor = torch.tensor(Y, dtype=torch.long)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_std, Y, test_size=0.3)
 
-    dataset = TensorDataset(data_tensor, label_tensor)
+    svm_classification = SVC(kernel='linear')
+    svm_classification.fit(X_train, Y_train)
 
-    train_size = int(0.9 * len(dataset))
-    test_size = len(dataset) - train_size
-
-    train_set, test_set = random_split(dataset, [train_size, test_size])
-
-    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
-    validate_loader = DataLoader(test_set, batch_size=32, shuffle=False)
-
-    # Create model
-    net = AlexNet(64, 100, 100)
-    net.to(device)
-
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.0002)
-    loss_function = nn.CrossEntropyLoss()
-    epochs = 10
-    # load model weights
-    save_path = "./AlexNet.pth"
-    best_acc = 0.0
-    train_steps = len(train_loader)
-
-    for epoch in range(epochs):
-        # train
-        net.train()
-        running_loss = 0.0
-        train_bar = tqdm(train_loader, file=sys.stdout)
-        for step, data in enumerate(train_bar):
-            images, labels = data
-            # images = data_transform(images)
-            print(images.shape)
-            optimizer.zero_grad()
-            outputs = net(images.to(device))
-            print(outputs)
-            loss = loss_function(outputs, labels.to(device))
-            loss.backward()
-            optimizer.step()
-
-            running_loss += loss.item()
-
-            # 设置进度条的描述信息
-            train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                     epochs,
-                                                                     loss)
-
-        # validate
-        net.eval()
-        acc = 0.0
-        with torch.no_grad():
-            val_bar = tqdm(validate_loader, file=sys.stdout)
-            for val_data in val_bar:
-                val_images, val_labels = val_data
-                outputs = net(val_images.to(device))
-                print(outputs)
+    print(f"分类准确率：{svm_classification.score(X_test, Y_test)}")
